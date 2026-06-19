ae7fcde1dffe45ec14a73d0897a8c5ddf29edbdc
sara20ziane <sara20ziane@gmail.com>
2026-06-19 20:39:51 +0100

    Add Firebase configuration

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCQ7MsDCFCIRJ6sBeg6UhoYUiGBA0cpyM8",
+  authDomain: "shopflow-dz.firebaseapp.com",
+  projectId: "shopflow-dz",
+  storageBucket: "shopflow-dz.firebasestorage.app",
+  messagingSenderId: "721551942224",
+  appId: "1:721551942224:web:b6287dec072f81d7656755"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
