c1360b26be1dc2f8967a76098f3eefe3dba00b74
sara20ziane <sara20ziane@gmail.com>
2026-06-21 11:31:50 +0100

    Switch to redesigned branded app

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import App from "./AppV2.jsx";
 import { setupPwaMetadata } from "./pwa.js";
 
 setupPwaMetadata();
