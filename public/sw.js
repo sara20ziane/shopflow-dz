6ce9fd1378ce51066954852a59ec498ceac69cc7
sara20ziane <sara20ziane@gmail.com>
2026-06-21 14:35:13 +0100

    Make service worker network only

--- public/sw.js
+++ public/sw.js
@@ -1,28 +1,17 @@
-const CACHE_NAME = "shopflow-dz-v2";
-const APP_SHELL = ["/", "/manifest.webmanifest", "/icon.svg"];
+const CACHE_FIX_VERSION = "cache-fix-2026-06-21";
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map((key) => (key === CACHE_NAME ? null : caches.delete(key))))
-    )
+    caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
   );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
-
-  event.respondWith(
-    fetch(event.request).catch(() =>
-      caches.match(event.request).then((cached) => cached || caches.match("/"))
-    )
-  );
+  event.respondWith(fetch(event.request));
 });
