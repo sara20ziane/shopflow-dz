5fc7688222bdf7ef2fab0ab9fe03a221eee5e27d
sara20ziane <sara20ziane@gmail.com>
2026-06-21 10:21:31 +0100

    Register PWA setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { setupPwaMetadata } from "./pwa.js";
+
+setupPwaMetadata();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
