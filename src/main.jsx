be47dc7b54e8a29b7d789c1d17485065747ff965
sara20ziane <sara20ziane@gmail.com>
2026-06-21 14:40:21 +0100

    Use UX clean app wrapper

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./AppCorrected.jsx";
+import App from "./AppUxClean.jsx";
 import { setupPwaMetadata } from "./pwa.js";
 
 setupPwaMetadata();
