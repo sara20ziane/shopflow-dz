b11b418fee2020e8b6175c80da40914f15dea9af
sara20ziane <sara20ziane@gmail.com>
2026-06-27 23:28:11 +0100

    Enable sales assistant app wrapper

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./AppUxClean.jsx";
+import App from "./AppWithSalesAssistant.jsx";
 import { setupPwaMetadata } from "./pwa.js";
 
 setupPwaMetadata();
