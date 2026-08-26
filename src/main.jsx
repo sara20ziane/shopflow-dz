3acaa57ab0604eaccacd4b2bc0031d65620c730f
sara20ziane <sara20ziane@gmail.com>
2026-08-26 09:56:28 +0100

    Route public business calculator page

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./AppWithSalesAssistant.jsx";
+import BusinessCalculatorPage from "./features/businessCalculator/BusinessCalculatorPage.jsx";
 import { setupPwaMetadata } from "./pwa.js";
 
 setupPwaMetadata();
 
+const normalizedPath = window.location.pathname.replace(/\/+$/, "") || "/";
+const isBusinessCalculator = ["/calculateur", "/calculateur-business", "/business-calculator"].includes(normalizedPath);
+const RootApp = isBusinessCalculator ? BusinessCalculatorPage : App;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RootApp />
   </React.StrictMode>
 );
